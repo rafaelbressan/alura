f1d1beaae4fc9dec0aab13a03142ef2f3b367696
Rafael Bressan <bressan.rafa@gmail.com>
2025-08-23 17:54:33 -0300

    greetings prompting

---
+++ js-dayweek-greeting/app.js
@@ -0,0 +1,25 @@
+const daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const weekend = daysOfTheWeek.slice(-2);
+const weekday = daysOfTheWeek.slice(0, 4);
+let dayOfTheWeek;
+const weekdayGreeting = "Have a great week!"
+const weekendGreeting = "Have a great weekend!"
+
+alert("Welcome!")
+
+function checkWeekdays(day) {
+    if (weekend.includes(day)) {
+        alert(weekendGreeting);
+    } else if (weekday.includes(day)) {
+        alert(weekdayGreeting)
+    } else {
+        alert("Not a valid day, choose between these dates:\n" + daysOfTheWeek.join(", "))
+    } console.log(day);
+}
+
+while (!dayOfTheWeek) {
+    let greetings = prompt("What day is it?");
+    dayOfTheWeek = greetings;
+    checkWeekdays(`${dayOfTheWeek}`);
+}
+
